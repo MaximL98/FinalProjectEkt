559322b4df518f9e09310bd23a4e507cea65a9d6
Maxim student <lebedinsky.maxim@gmail.com>
2022-12-12 21:57:50 +0200

    now customer extends new logic class "SystemUser"

--- Client/finalProjectEkt_Client/src/logic/Customer.java
+++ Client/finalProjectEkt_Client/src/logic/Customer.java
@@ -1,73 +1,42 @@
 package logic;
 
-import java.io.Serializable;
+//import java.io.Serializable;
 
-public class Customer implements Serializable{
+public class Customer extends SystemUser{
 
-	private static final long serialVersionUID = 1L;
-	//updated costumer logic part, 
-	private String firstName;
-	private String lastName;
-	private Integer id;
-	private String phoneNumber;
-	private String emailAddress;
-	private String creditCard;
+	//private static final long serialVersionUID = 1L;
+		
 	//if customer don't have subscriber number
 	private String subscriberNumber = null;
 
-	
+	//customer constructor, inherits fields from system user
 	public Customer(String firstName, String lastName, Integer id, String phoneNumber, String emailAddress,
-			String creditCard) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.id = id;
-		this.phoneNumber = phoneNumber;
-		this.emailAddress = emailAddress;
-		this.creditCard = creditCard;
-		//i guess this field don't have to be in the constructor
-		//this.subscriberNumber = subscriberNumber;
+			String creditCard, String username, String password) {
+		super(firstName, lastName, id, phoneNumber, emailAddress, creditCard, username, password);
 	}
 
-	public String getFirstName() {return firstName;}
-
-	public void setFirstName(String firstName) {this.firstName = firstName;}
-
-	public String getLastName() {return lastName;}
-
-	public void setLastName(String lastName) {this.lastName = lastName;}
-
-	public Integer getId() {return id;}
-
-	public void setId(Integer id) {this.id = id;}
-
-	public String getPhoneNumber() {return phoneNumber;}
-
-	public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
-
-	public String getEmailAddress() {return emailAddress;}
-
-	public void setEmailAddress(String emailAddress) {this.emailAddress = emailAddress;}
-
-	public String getCreditCard() {return creditCard;}
-
-	public void setCreditCard(String creditCard) {this.creditCard = creditCard;}
-
+	//getting and setting customer subscription number
 	public String getSubscriberNumber() {return subscriberNumber;}
 
 	public void setSubscriberNumber(String subscriberNumber) {
 		this.subscriberNumber = subscriberNumber; 
 	}
 	
-	// just for convenience
+	//if he is a subscriber return the number
 	public boolean isSubscriber() {return subscriberNumber != null;}
 
+	//toString method that returns customer details
 	@Override
 	public String toString() {
-		return "Customer [first Name = " + firstName + ", last Name = " + lastName + ", id= " + id + ", phoneNumber = "
-				+ phoneNumber + ", emailAddress = " + emailAddress + ", creditCard = " + creditCard + ", subscriberNumber = "
-				+ subscriberNumber + "]";
+		return "Customer [subscriberNumber=" + subscriberNumber + ", getSubscriberNumber()=" + getSubscriberNumber()
+				+ ", isSubscriber()=" + isSubscriber() + ", getFirstName()=" + getFirstName() + ", getLastName()="
+				+ getLastName() + ", getId()=" + getId() + ", getPhoneNumber()=" + getPhoneNumber()
+				+ ", getEmailAddress()=" + getEmailAddress() + ", getCreditCard()=" + getCreditCard()
+				+ ", getUsername()=" + getUsername() + ", getPassword()=" + getPassword() + ", toString()="
+				+ super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
 	}
+
+	
 	
 	
 }

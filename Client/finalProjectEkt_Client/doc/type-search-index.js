36d29d449ae6f516bc6d0dd0b5805e4ae7c69fba
lmaxp <lmaxp@LAPTOP-AVQR9A0A.local>
2022-12-16 13:54:10 +0200

    fixed: javadoc not showing "systemUser"

--- Client/finalProjectEkt_Client/doc/type-search-index.js
+++ Client/finalProjectEkt_Client/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"logic","l":"Customer"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"logic","l":"SystemUser"}];updateSearchResults();

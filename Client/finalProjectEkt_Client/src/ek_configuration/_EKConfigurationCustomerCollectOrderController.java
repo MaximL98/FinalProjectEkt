42f56389bf728e46025690e3fdf69196154e8f60
rotemlv <90067343+rotemlv@users.noreply.github.com>
2023-01-12 16:14:08 +0200

    Implemented collecting order from a machine (EK conf). Now customer can login in EK and see all orders made to the specific machine, and collect one. Missing part!!!::: If no orders exist, we need a pop-up in initialize(), now it just shows an empty table.

--- Client/finalProjectEkt_Client/src/ek_configuration/_EKConfigurationCustomerCollectOrderController.java
+++ Client/finalProjectEkt_Client/src/ek_configuration/_EKConfigurationCustomerCollectOrderController.java
@@ -1,15 +1,25 @@
 package ek_configuration;
 
+import java.util.ArrayList;
+
+import client.ClientController;
+import client.ClientUI;
+import common.SCCP;
+import common.ServerClientRequestTypes;
+import common.WindowStarter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class _EKConfigurationCustomerCollectOrderController {
 	
-	private String[] exampleContents = {"ass", "tities", "pizza", "shawarma", "pastrama"};
+	// lololol (changed it to be proper)
+	ArrayList<String> existingOrdersContents = new ArrayList<>();
 	
     @FXML
     private Button btnFinishCollectOrder;
@@ -28,21 +38,50 @@ public class _EKConfigurationCustomerCollectOrderController {
     	
     	// TODO: send query to get all orders from this customer that can be picked up on this machine
     	// (replace the argument in the following line with the result of a query)
-    	ordersByID.getItems().addAll(exampleContents);
-    	
+    	// select * from orders JOIN customer_orders on orders.orderID=customer_orders.orderId WHERE statusId=1 AND typeId = 1;
+    	ClientUI.clientController.accept(new SCCP(ServerClientRequestTypes.SELECT, 
+    			new Object[] {"orders JOIN customer_orders on orders.orderID=customer_orders.orderId", 
+    					true, "orders.orderID", 
+    					true, "customerId="+ClientController.getCurrentSystemUser().getId()+
+    					" AND orders.statusId=1 AND orders.typeId=1 AND orders.machineID="+ClientController._EkCurrentMachineID, false, null}));
+    	@SuppressWarnings("unchecked")
+		ArrayList<ArrayList<Object>> res = (ArrayList<ArrayList<Object>>) ClientController.responseFromServer.getMessageSent();
+    	for(ArrayList<Object> row : res) {
+    		if(row.size() > 0) {
+    			existingOrdersContents.add(row.get(0).toString());
+    		}
+    	}
+    	ordersByID.getItems().addAll(existingOrdersContents);
+    	if(existingOrdersContents.size() == 0) {
+    		// show a pop up that lets the user know he has no open orders. return user to previous page!
+    		System.out.println("IMPLEMENT THIS!");
+    	}
     	
     }
     
     @FXML
     void getBtnFinishCollectOrder(ActionEvent event) {
-    	System.out.println("Selected: " + ordersByID.getSelectionModel().getSelectedItem());
+    	String orderIdString =  ordersByID.getSelectionModel().getSelectedItem();
+    	System.out.println("Selected: " +orderIdString);
+    	
     	System.out.println("Processing . . .");
+    	// send this query:
+    	ClientUI.clientController.accept(new SCCP(ServerClientRequestTypes.UPDATE, new Object[] {"orders", "statusId=3", "orderID="+orderIdString}));
     	System.out.println("Your order has been dispensed, waiting for you to collect your items.");
     	
     	// TODO:
     	// insert a query to set the order status of the selected order to "completed".
     	
     	System.out.println("Enjoy!");
+    	// load previous page:
+		// sammy D the current window
+		((Node)event.getSource()).getScene().getWindow().hide();
+		// prepare the new stage:
+		Stage primaryStage = new Stage();
+		String nextScreenPath = "/gui/_EKConfigurationCustomerHomeArea.fxml";
+		String nextPathTitle = "Customer Home Frame";
+		WindowStarter.createWindow(primaryStage, this, nextScreenPath, null, nextPathTitle);
+		primaryStage.show();
     }
 
 }

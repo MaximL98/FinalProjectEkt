c87d6a4b3be8a87c1d65fade6411866574875144
nastya2022-ch <116434662+nastya2022-ch@users.noreply.github.com>
2023-01-17 13:07:50 +0200

    more javadoc

--- Client/finalProjectEkt_Client/src/controllers/EktCancelOrderPageController.java
+++ Client/finalProjectEkt_Client/src/controllers/EktCancelOrderPageController.java
@@ -3,14 +3,11 @@ package controllers;
 import java.sql.Date;
 import java.util.ArrayList;
 
-import org.junit.jupiter.params.shadow.com.univocity.parsers.common.record.Record;
-
 import client.ClientController;
 import client.ClientUI;
 import common.SCCP;
 import common.ServerClientRequestTypes;
 import common.WindowStarter;
-import controllers.EktReportDisplayPageController.productInTable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,45 +22,77 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-
+/**
+ * A class that represents an order to be cancelled.
+ * It contains variables for the ID, total price, and date of the order.
+ *
+ */
 public class EktCancelOrderPageController {
 
 	public class orderToCancel {
 		private SimpleStringProperty id;
 		private SimpleStringProperty totalPrice;
 		private SimpleStringProperty date;
-
+		/**
+		 * Constructor for orderToCancel class.
+		 * 
+		 * @param id the ID of the order
+		 * @param totalPrice the total price of the order
+		 * @param date the date the order was received
+		 */
 		public orderToCancel(int id, double totalPrice, String date) {
 			this.id = new SimpleStringProperty(new Integer(id).toString());
 			this.totalPrice = new SimpleStringProperty(new Double(totalPrice).toString());
 			this.date = new SimpleStringProperty(date);
 		}
-		
+		/**
+		 * Gets the total price of the order.
+		 * 
+		 * @return total price of the order
+		 */
 		public SimpleStringProperty getTotalPrice() {
 			return totalPrice;
 		}
+		/**
+		 * Sets the total price of the order.
+		 * 
+		 * @param totalPrice the new total price of the order
+		 */
 
 		public void setTotalPrice(SimpleStringProperty totalPrice) {
 			this.totalPrice = totalPrice;
 		}
-
+		/**
+		 * Gets the date the order was received.
+		 * 
+		 * @return date the order was received
+		 */
 		public SimpleStringProperty getDate() {
 			return date;
 		}
+		/**
 
+		Sets the date of the order.
+		@param date the new date of the order
+		*/
 		public void setDate(SimpleStringProperty customerName) {
 			this.date = customerName;
 		}
-
+		/**
+		 * Gets the ID of the order.
+		 * 
+		 * @return ID of the order
+		 */
 		public SimpleStringProperty getId() {
 			return id;
-		}
+		}	/**
+		 * Sets the ID of the order.
+		 * 
+		 * @param id the new ID of the order
+		 */
 
 		public void setId(SimpleStringProperty id) {
 			this.id = id;
@@ -93,7 +122,10 @@ public class EktCancelOrderPageController {
 	private TableView<orderToCancel> tableUsers;
 	
 	@FXML
-
+	/**
+	 * A method that is called when the page is initialized.
+	 * It sets up the table columns and populates the table with orders to be cancelled.
+	 */
 	@SuppressWarnings("unchecked")
 	public void initialize() {
 		final ObservableList<orderToCancel> data = FXCollections.observableArrayList();
@@ -128,6 +160,12 @@ public class EktCancelOrderPageController {
 			cancel.setTextFill(Color.WHITE);
 			cancel.getStylesheets().add("/gui/buttonCSS.css");
 			TableCell<orderToCancel, Button> cell = new TableCell<orderToCancel, Button>() {
+				/**
+				 * A method that is called when the btnUpdate button is clicked.
+				 * It updates the list of orders to be cancelled in the table view.
+				 * 
+				 * @param event the event that triggered the method call
+				 */
 				@Override
 				public void updateItem(Button item, boolean empty) {
 					super.updateItem(item, empty);
@@ -195,7 +233,12 @@ public class EktCancelOrderPageController {
 		tableUsers.getColumns().add(columnAccept);
 
 	}
-
+	/**
+	* A method that is called when the btnBack button is clicked.
+	* It returns the user to the previous page.
+	*
+	* @param event the event that triggered the method call
+	*/
 	@FXML
 	void getBtnBack(ActionEvent event) {
 		Stage primaryStage = new Stage();
--- Client/finalProjectEkt_Client/src/controllers/EktDivisionManagerHomePageController.java
+++ Client/finalProjectEkt_Client/src/controllers/EktDivisionManagerHomePageController.java
@@ -11,7 +11,14 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+/**
 
+This class is the controller for the Division Manager home page.
+It contains methods for handling button events and updating the UI.
+@author [Raz]
+@version [1.0]
+@since [12.1.23]
+*/
 public class EktDivisionManagerHomePageController {
     @FXML
     private Button btnLogout;
@@ -24,13 +31,22 @@ public class EktDivisionManagerHomePageController {
     
     @FXML
     private Button btnAddUserToDB;
-
+    /**
+     * Initializes the UI by setting the welcome text to display the current user's first name
+     * and centering the text
+     */
     @FXML
     public void initialize() {
     	txtGreeting.setText("WELCOME BACK, " + ClientController.getCurrentSystemUser().getFirstName());
     	txtGreeting.setLayoutX(400 - (txtGreeting.minWidth(0))/ 2);
     }
-    
+    /**
+     * Handles the event when the "View Reports" button is clicked.
+     * Opens a new window for the report selection form.
+     * The primary window is closed after the new window is opened.
+     * 
+     * @param event the action event that triggered the method call
+     */
     @FXML
     public void getBtnViewReports(ActionEvent event) {
     	Stage primaryStage = new Stage();
@@ -38,11 +54,17 @@ public class EktDivisionManagerHomePageController {
     	primaryStage.show();
     	((Stage) ((Node) event.getSource()).getScene().getWindow()).close(); // closing primary window
     }
-    
+
+/**
+ * Handles the event when the "Logout" button is clicked.
+ * Sends a logout request to the server and opens a new window for the login form.
+ * The primary window is closed after the new window is opened.
+ * 
+ * @param event the action event that triggered the method call
+ */
     @FXML
     void getBtnLogout(ActionEvent event) {
     	Stage primaryStage = new Stage();
-    	// bihiat ze lifnei!!!!
     	ClientUI.clientController.accept(new SCCP(ServerClientRequestTypes.LOGOUT, ClientController.getCurrentSystemUser().getUsername()));
     	ClientController.sendLogoutRequest();
     	WindowStarter.createWindow(primaryStage, this, "/gui/EktSystemUserLoginForm.fxml", null, "Login", true);
@@ -50,7 +72,12 @@ public class EktDivisionManagerHomePageController {
     	((Stage) ((Node) event.getSource()).getScene().getWindow()).close(); // closing primary window
     	
     }
-    
+    /**
+     * Handles the event when the "Add User To Database" button is clicked.
+     * Opens a new window for the "Add User To Database" form.
+     * The primary window is closed after the new window is opened.
+     @param event the action event that triggered the method call
+*/
     @FXML
     public void getBtnAddUserToDB(ActionEvent event) {
     	Stage primaryStage = new Stage();
